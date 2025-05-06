9283e77af0c384fa8fbd828a320aca4e896e8e5e
reg734 <33149497+reg734@users.noreply.github.com>
2025-05-06 21:21:04 +0800

    加入新檔案或功能

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return  render_template('index.html')
-#
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
-# @app.route('/')
-# def show_weathers():
-#     weather_list = ["sunny", "rainy", "cloudy", "sunny", "rainy"]
-#     return render_template('weather.html', weather_list=weather_list)
+line_bot_api = LineBotApi(os.getenv('YOUR_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('YOUR_CHANNEL_SECRET'))
 
-if __name__ == '__main__':
-    app.run()
+# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+def generate_response(prompt, role="user"):
+    pass
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
